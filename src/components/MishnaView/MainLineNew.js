5e78ffa290c91ed404ec47ef5a62456c09887e78
Yaron Bar <rugggger@yahoo.co.uk>
2021-05-31 20:44:47 +0300

    removed redundant code

--- src/components/MishnaView/MainLineNew.js
+++ src/components/MishnaView/MainLineNew.js
@@ -1,19 +1,10 @@
 import React from "react";
- import { makeStyles } from '@material-ui/core/styles';
 import  SublineDisplay  from "./SublineDisplay";
-// import Tooltip from '@material-ui/core/Tooltip';
-const useStyles = makeStyles((theme) => {
-  return {
-    root: {
-      width: "100%",
-      direction: "rtl"
-    },
-  }
-})
+
 
 export const MainLineNew = (props)=>{
   const { line, lineIndex } =  props;
-  const classes = useStyles();
+
 
   return (
     <>
