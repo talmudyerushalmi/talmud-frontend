6d17a7c93975ef958625ca839789cbc81b2fed01
Yaron Bar <rugggger@yahoo.co.uk>
2021-11-05 07:02:48 +0200

    diff

--- src/components/MishnaView/SugiaButton.tsx
+++ src/components/MishnaView/SugiaButton.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import { iLine, iMishna } from "../../types/types";
+import { iLine, iMishna, iSubline } from "../../types/types";
 import { connect } from "react-redux";
 import { selectSublines } from "../../store/actions";
 import { getSugiaLines } from "../../inc/mishnaUtils";
+import * as _ from 'lodash';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,28 +30,32 @@ const mapDispatchToProps = (dispatch:any) => ({
   })
 
 const mapStateToProps = state => ({
+    selectedSublines: state.mishnaView.selectedSublines,
     currentMishna: state.general.currentMishna,
 })
 interface Props {
     index: number;
     line: iLine;
     selectSublines: Function;
+    selectedSublines: iSubline[];
     currentMishna: iMishna;
 }
 const SugiaButton = (props: Props) => {
   const classes = useStyles();
-  const { index, line, selectSublines, currentMishna } = props;
+  const { index, line, selectSublines, currentMishna, selectedSublines } = props;
   let l = line?.sublines ? line?.sublines[0] : null;
 
   const selectSugiaHandler = ()=>{
       const sugiaSublines = getSugiaLines(currentMishna, line);
-      selectSublines(sugiaSublines)
+      const diff = _.difference(sugiaSublines, selectedSublines);
+      if (diff.length===0){
+        selectSublines([])
+      } else {
+        selectSublines(sugiaSublines)
+      }
   }
 
-  useEffect(()=>{
-     l = line?.sublines ? line?.sublines[0] : null;
-  }, [line])
-
+  
   return (
     <button 
     onClick={selectSugiaHandler}
