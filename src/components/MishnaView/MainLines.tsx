41bb2fa7518c30eb88a935e7d0f1067dd5744e07
Yaron Bar <rugggger@yahoo.co.uk>
2021-11-15 18:48:01 +0200

    add link button for admin edit in mishnaView/lines

--- src/components/MishnaView/MainLines.tsx
+++ src/components/MishnaView/MainLines.tsx
@@ -3,6 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import MainLine from "./MainLine";
 import { iLine } from "../../types/types";
 import SugiaButton from "./SugiaButton";
+import { useHistory, useParams } from "react-router-dom";
+import { IconButton } from "@material-ui/core";
+import { Edit } from "@material-ui/icons";
+import { routeObject } from "../../routes/AdminRoutes";
+import { connect } from "react-redux";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
@@ -15,16 +20,31 @@ const useStyles = makeStyles((theme) => ({
     //@ts-ignore
     ...theme.typography.sourceReference,
   },
+  lines: {
+    position: 'relative'
+  },
+  adminButton: {
+    position: "absolute",
+    left: '-3rem',
+    top: '-1rem'
+  },
 }));
+
+const mapStateToProps = (state: any) => ({
+  username: state.authentication.username,
+});
 
 interface Props {
   lines: iLine[];
+  username: any;
 }
 const MainLines = (props: Props) => {
   const classes = useStyles();
-  const { lines } = props;
+  const { lines, username } = props;
   let sectionsIndex = 1;
 
+  const history = useHistory();
+  const route = useParams<routeObject>();
   if (!lines) {
     return null;
   }
@@ -33,7 +53,19 @@ const MainLines = (props: Props) => {
     <div className={classes.root}>
       {lines.map((line, index) => {
         return (
-          <div key={line.lineNumber}>
+          <div key={line.lineNumber} className={classes.lines}>
+            {username ? (
+              <IconButton
+                className={classes.adminButton}
+                onClick={() => {
+                  history.push(
+                    `/admin/edit/${route.tractate}/${route.chapter}/${route.mishna}/${line.lineNumber}/`
+                  );
+                }}
+              >
+                <Edit></Edit>
+              </IconButton>
+            ) : null}
             {line.sugiaName ? (
               <SugiaButton index={sectionsIndex++} line={line} />
             ) : null}
@@ -45,4 +77,4 @@ const MainLines = (props: Props) => {
   );
 };
 
-export default MainLines;
+export default connect(mapStateToProps)(MainLines);
