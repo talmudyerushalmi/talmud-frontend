c8375e0fc09973deea86919d46c06a7a21f4a991
Yaron Bar <rugggger@yahoo.co.uk>
2022-01-16 20:31:26 +0200

    gtm

--- src/pages/ViewMishnaPage.tsx
+++ src/pages/ViewMishnaPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer } from "../layout/Footer";
 import {
   PageContent,
@@ -10,6 +10,15 @@ import TagManager from 'react-gtm-module';
 
 
 const ViewMishnaPage = () => {
+  useEffect(()=>{
+    TagManager.dataLayer({
+      dataLayer: {
+        event: 'page_view',
+        pagePath: window.location.href,
+        pageTitle: 'mishna-view',
+      },
+    });
+  },[])
   return (
     <PageWithNavigation linkPrefix="/talmud" afterNavigateHandler={()=>{
       TagManager.dataLayer({
