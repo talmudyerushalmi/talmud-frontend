a9d972ca2e0da7e15a665a8f87c1433b14a0d2e8
Yaron Bar <rugggger@yahoo.co.uk>
2022-01-16 20:20:36 +0200

    gtm mishna view

--- src/pages/ViewMishnaPage.tsx
+++ src/pages/ViewMishnaPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Footer } from "../layout/Footer";
 import {
   PageContent,
@@ -10,18 +10,16 @@ import TagManager from 'react-gtm-module';
 
 
 const ViewMishnaPage = () => {
-  useEffect(()=>{
-    TagManager.dataLayer({
-      dataLayer: {
-        event: 'page_view',
-        pagePath: window.location.href,
-        pageTitle: 'mishna-view',
-      },
-    });
-  },[])
-
   return (
-    <PageWithNavigation linkPrefix="/talmud" afterNavigateHandler={()=>{window.scrollTo(0,0)}}>
+    <PageWithNavigation linkPrefix="/talmud" afterNavigateHandler={()=>{
+      TagManager.dataLayer({
+        dataLayer: {
+          event: 'page_view',
+          pagePath: window.location.href,
+          pageTitle: 'mishna-view',
+        },
+      });
+      window.scrollTo(0,0)}}>
       <PageHeader>
       </PageHeader>
       <PageContent>
