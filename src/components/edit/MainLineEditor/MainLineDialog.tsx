64c3a5e1027be2e949147ab0b348f042490e074e
Yaron Bar <rugggger@yahoo.co.uk>
2022-02-22 23:15:15 +0200

    updated correction entity

--- src/components/edit/MainLineEditor/MainLineDialog.tsx
+++ src/components/edit/MainLineEditor/MainLineDialog.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { EditingData } from './MainLineEditor';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { EditingData } from "./MainLineEditor";
 
-export enum NosachEntity  {
-  ADD = 'ADD',
-  DELETE = 'DELETE',
-  QUOTE = 'QUOTE',
-  CORRECTION = 'CORRECTION'
+export enum NosachEntity {
+  ADD = "ADD",
+  DELETE = "DELETE",
+  QUOTE = "QUOTE",
+  CORRECTION = "CORRECTION",
 }
 export const NosachMap = new Map([
   [
@@ -28,99 +35,115 @@ export const NosachMap = new Map([
       title: "ציטוט",
     },
   ],
-  
-
 ]);
-export interface InitialEntityDialogState{
-  type: NosachEntity,
-  editingData: EditingData
+export interface InitialEntityDialogState {
+  type: NosachEntity;
+  editingData: EditingData;
 }
-
 
 export interface Props {
   initialState: InitialEntityDialogState;
   open: boolean;
-  onSaveEntity: (type: NosachEntity, editingData: EditingData)=>void;
+  onSaveEntity: (type: NosachEntity, editingData: EditingData) => void;
   onClose: () => void;
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)'
-
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
   },
-
 }));
 
-export const MainLineDialog = (props: Props)=>{
+export const MainLineDialog = (props: Props) => {
   const classes = useStyles();
   const { onClose, onSaveEntity, open, initialState } = props;
   const { type } = initialState;
-  const [editingData, setEditingData] = useState<EditingData>(initialState.editingData)
+  const [editingData, setEditingData] = useState<EditingData>(
+    initialState.editingData
+  );
 
   const handleClose = () => {
     onClose();
   };
 
-
-  useEffect(()=>{
-    setEditingData(initialState.editingData)
-  },[initialState])
+  useEffect(() => {
+    setEditingData(initialState.editingData);
+  }, [initialState]);
 
   return (
-    <Dialog 
-    style={{direction:'rtl'}}
-    BackdropProps= {{
-      classes: {
-          root: classes.root
-      }}}
-    onClose={handleClose} open={open}>
+    <Dialog
+      style={{ direction: "rtl" }}
+      BackdropProps={{
+        classes: {
+          root: classes.root,
+        },
+      }}
+      onClose={handleClose}
+      open={open}
+    >
       <DialogTitle>{NosachMap.get(initialState.type)?.title}</DialogTitle>
-      <DialogContent>        
-      <TextField
+      <DialogContent>
+        <TextField
           id="outlined-multiline-flexible"
           label="הערות עריכה"
           multiline
           maxRows={4}
           value={editingData.editingComment}
-          onChange={(e)=>setEditingData({...editingData, editingComment:e.target.value})}
+          onChange={(e) =>
+            setEditingData({ ...editingData, editingComment: e.target.value })
+          }
         />
-            {
-          type === NosachEntity.CORRECTION ?
+        {type === NosachEntity.CORRECTION ? (
+          <>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="נוסח מקורי"
+              multiline
+              maxRows={4}
+              value={editingData.oldWord}
+              onChange={(e) =>
+                setEditingData({ ...editingData, oldWord: e.target.value })
+              }
+            />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="נוסח מתוקן"
+              multiline
+              maxRows={4}
+              value={editingData.newWord}
+              onChange={(e) =>
+                setEditingData({ ...editingData, newWord: e.target.value })
+              }
+            />
+          </>
+        ) : null}
+        {type === NosachEntity.QUOTE ? (
           <TextField
-          id="outlined-multiline-flexible"
-          label="נוסח מקורי"
-          multiline
-          maxRows={4}
-          value={editingData.oldWord}
-          onChange={(e)=>setEditingData({...editingData, oldWord:e.target.value})}
-        /> :
-        null
-        }
-        {
-          type === NosachEntity.QUOTE ?
-          <TextField
-          id="outlined-multiline-flexible"
-          label="הפניה"
-          multiline
-          maxRows={4}
-          value={editingData.linkTo}
-          onChange={(e)=>setEditingData({...editingData, linkTo:e.target.value})}
-        /> :
-        null
-        }
+            id="outlined-multiline-flexible"
+            label="הפניה"
+            multiline
+            maxRows={4}
+            value={editingData.linkTo}
+            onChange={(e) =>
+              setEditingData({ ...editingData, linkTo: e.target.value })
+            }
+          />
+        ) : null}
       </DialogContent>
       <DialogActions>
-      <Button 
-      variant="contained"
-      onClick={()=>{
-        onSaveEntity(initialState.type,{...editingData})
-        }}>הוסף</Button>
-      <Button onClick={onClose}>בטל</Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            onSaveEntity(initialState.type, { ...editingData });
+          }}
+        >
+          הוסף
+        </Button>
+        <Button onClick={onClose}>בטל</Button>
       </DialogActions>
     </Dialog>
   );
-}
+};
 
 // export default function SimpleDialogDemo() {
 //   const [open, setOpen] = React.useState(false);
