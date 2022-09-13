f9dfb8c64fbddea66f48b41791d40a4f8ae5c10d
Yaron Bar <rugggger@yahoo.co.uk>
2022-09-13 10:50:31 +0300

    added cancel to mishna request

--- src/services/NavigationService.ts
+++ src/services/NavigationService.ts
@@ -3,9 +3,11 @@ import axiosInstance from './api';
 
 
 export default class NavigationService {
-  static async getMishnaForNavigation(tractate, chapter, mishna): Promise<iMishnaForNavigation> {
+  static async getMishnaForNavigation(tractate, chapter, mishna, controller: AbortController): Promise<iMishnaForNavigation> {
     const url = `/navigation/${tractate}/${chapter}/${mishna}`;
-    const response = await axiosInstance.get(url);
+    const response = await axiosInstance.get(url, {
+      signal: controller.signal
+    });
     return response.data;
   }
 }
