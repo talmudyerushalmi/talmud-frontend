dfe0bdf8f8241b295234886270dd4c65ad379c7c
hadarhubara10 <hadarhubara10@gmail.com>
2022-12-15 17:53:11 +0200

    fix synopsisCode

--- src/inc/manuscriptUtils.ts
+++ src/inc/manuscriptUtils.ts
@@ -4,19 +4,19 @@ export const getImageUrl = (manuscripts: iManuscript[], sublineData: iManuscript
   return (
     manuscripts?.find(
       (manuscript) =>
-        manuscript?.slug === 'leiden' &&
+        manuscript?.slug === sublineData?.synopsisCode &&
         manuscript?.fromLine < sublineData?.line &&
         manuscript?.toLine > sublineData?.line
     )?.imageurl ||
     manuscripts?.find(
       (manuscript) =>
-        manuscript?.slug === 'leiden' &&
+        manuscript?.slug === sublineData?.synopsisCode &&
         manuscript?.toLine === sublineData?.line &&
         manuscript?.toSubline >= sublineData?.subline?.index
     )?.imageurl ||
     manuscripts?.find(
       (manuscript) =>
-        manuscript?.slug === 'leiden' &&
+        manuscript?.slug === sublineData?.synopsisCode &&
         manuscript?.fromLine === sublineData?.line &&
         manuscript?.fromSubline <= sublineData?.subline?.index
     )?.imageurl ||
