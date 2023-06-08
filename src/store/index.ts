264c20e5cae647a84b2e50146dd0a342fef85751
Yaron Bar <bar.yaron2018@gmail.com>
2023-06-08 20:02:19 +0300

    Comments (#37)
    * service
    
    * CommentService
    
    * get private comments
    
    * redux
    
    * excerpt accordion
    
    * comments section
    
    * comments
    
    * filter
    
    * popup
    
    * comments correct lines
    
    * edit comment
    
    * edit comment
    
    * get, delete and update
    
    * create comment
    
    * clear form
    
    * public comments
    
    * ModerationCommentsPage
    
    * CommentsAdminPage
    
    * show comments when the user is logged in
    
    * textUtils new line
    
    * app todo
    
    * comment per line
    
    * types
    
    * types V2
    
    * tractate is required, and changed iComment to  iUser
    
    ---------
    
    Co-authored-by: hadarhubara10 <hadarhubara10@gmail.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducers/authReducer';
 import mishnaViewReducer from './reducers/mishnaViewReducer';
 import navigationReducer from './reducers/navigationReducer';
 import mishnaEditReducer from './reducers/mishnaEditReducer';
 import generalReducer from './reducers/generalReducer';
+import commentsReducer from './reducers/commentsReducer';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import relatedReducer from './reducers/relatedReducer';
-import storage from 'redux-persist/lib/storage'
-import {
-  persistReducer,
-} from 'redux-persist'
-import { setupListeners } from '@reduxjs/toolkit/query'
+
 
 
 const rootReducer = combineReducers({
@@ -19,32 +19,29 @@ const rootReducer = combineReducers({
   authentication: authReducer,
   mishnaView: mishnaViewReducer,
   mishnaEdit: mishnaEditReducer,
+  comments: commentsReducer,
   related: relatedReducer,
 });
-
 
 const persistConfig = {
   key: 'root',
   storage: storage,
   whitelist: ['mishnaView'],
-}
+};
 
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-})
-
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-
-setupListeners(store.dispatch)
-export default store
+setupListeners(store.dispatch);
+export default store;
