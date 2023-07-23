bac43664fdcd32b2fd1396444b4e96020913c4c2
Joshua Ezra Brunnlehrman <134544831+Brunnlehrman@users.noreply.github.com>
2023-07-23 15:48:42 +0300

    decrease spaces in drop down between entries

--- src/components/MishnaView/exercptsAccordion.tsx
+++ src/components/MishnaView/exercptsAccordion.tsx
@@ -12,13 +12,17 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '2rem',
   },
   rootExpanded: {
-    maxHeight: '200px',
+    //drop down label stays up top, does not expand
+    '& .muirtl-1ibip0b-MuiButtonBase-root-MuiAccordionSummary-root.Mui-expanded': {minHeight: '1px'},
+    //decrease spaces in drop down between entries
+    '& .muirtl-sh22l5-MuiButtonBase-root-MuiAccordionSummary-root': {minHeight: '1px'},
+    '& .muirtl-o4b71y-MuiAccordionSummary-content': {margin: '1px'},
     overflow: 'hidden',
     '&.MuiPaper-root.MuiAccordion-root': { backgroundColor: 'rgba(0, 0, 0, .03)' },
     '&.MuiPaper-root.MuiPaper-elevation.MuiPaper-elevation1.MuiAccordion-root.Mui-expanded.MuiAccordion-gutters.makeStyles-rootExpanded-12.muirtl-t4qmgb-MuiPaper-root-MuiAccordion-root': {margin: '1px'},
     flexGrow: 1,
     '& >  .MuiCollapse-root': {
-      height: '50% !important',
+      height: '100% !important',
       overflow: 'scroll',
     },
     '& > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > div > .MuiAccordionDetails-root': {
