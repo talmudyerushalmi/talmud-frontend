46cc105ca881cea3062765b98bd1454c7210ab03
Hadar Hubara <76662798+hadarhubara10@users.noreply.github.com>
2025-08-13 15:34:34 +0300

    fix: navigation performance (#82)
    * Fix navigation performance and remove unnecessary call
    
    * Update ChooseMishnaBar.tsx
    
    * Update ChooseMishnaBar.tsx

--- src/components/shared/ChooseMishna/ChooseMishnaBar.tsx
+++ src/components/shared/ChooseMishna/ChooseMishnaBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Grid, Box } from '@mui/material';
 
 import { useParams } from 'react-router';
@@ -7,24 +7,17 @@ import ChooseMishnaForm from './ChooseMishnaForm';
 import { PrintHeader } from '../PrintHeader';
 import { routeObject } from '../../../store/reducers/navigationReducer';
 import { iLink, iTractate } from '../../../types/types';
-import { connect } from 'react-redux';
-import { setRoute } from '../../../store/actions/navigationActions';
 import SearchBar from './SearchBar';
 import PageService from '../../../services/pageService';
+import { useAppDispatch } from '../../../app/hooks';
+import { setRoute } from '../../../store/actions/navigationActions';
 
 interface Props {
   allChapterAllowed?: boolean;
   keypressNavigation?: boolean;
   onNavigationUpdated: Function;
   onButtonNavigation?: (nav: iLink) => void;
-  setRoute: (tractate: string, chapter: string, mishna: string, line: string) => void;
 }
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  setRoute: (tractate: string, chapter: string, mishna: string, line: string) => {
-    dispatch(setRoute(tractate, chapter, mishna, line));
-  },
-});
 
 const selectButtonDisabled = () => false;
 
@@ -33,8 +26,8 @@ const ChooseMishnaBar = ({
   keypressNavigation = false,
   onNavigationUpdated,
   onButtonNavigation = () => {},
-  setRoute,
 }: Props) => {
+  const dispatch = useAppDispatch();
   const { tractate, chapter, mishna, line } = useParams<routeObject>();
   const [navigation, setNavigation] = useState<iLink>({
     tractate: tractate || '',
@@ -52,20 +45,12 @@ const ChooseMishnaBar = ({
   const handleNavigate = (e) => {
     onNavigationUpdated(navigation);
   };
-  const memoizedProps = useMemo(() => {
-    const link: iLink = {
-      tractate: tractate || '',
-      chapter: chapter || '',
-      mishna: mishna || '',
-      lineNumber: line || '',
-    };
-    if (tractate && chapter && mishna) {
-      setRoute(tractate, chapter, mishna, line || '');
+
+  useEffect(() => {
+    if (navigation.tractate && navigation.chapter && navigation.mishna) {
+      dispatch(setRoute(navigation.tractate, navigation.chapter, navigation.mishna, navigation.lineNumber));
     }
-    return {
-      initValues: link,
-    };
-  }, [tractate, chapter, mishna, line]);
+  }, [navigation, dispatch]);
 
   return (
     <>
@@ -84,7 +69,7 @@ const ChooseMishnaBar = ({
                 setNavigation(newNav);
               }}
               onButtonNavigation={onButtonNavigation}
-              {...memoizedProps}
+              initValues={navigation}
               allTractates={allTractates}
             />
           </Box>
@@ -108,4 +93,4 @@ const ChooseMishnaBar = ({
   );
 };
 
-export default connect(() => ({}), mapDispatchToProps)(ChooseMishnaBar);
+export default ChooseMishnaBar;
