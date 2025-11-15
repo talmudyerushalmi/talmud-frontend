24c9c05ef28a01bc4ac738bc4c62740342922cdb
Asaf Corem <asaelcorem@gmail.com>
2025-11-15 08:30:57 +0200

    minimize the gaps between lines in the side accordion

--- src/components/MishnaView/exercptsAccordion.tsx
+++ src/components/MishnaView/exercptsAccordion.tsx
@@ -5,15 +5,39 @@ import Accordion from '@mui/material/Accordion';
 const useStyles = makeStyles((theme) => ({
   root: {
     overflow: 'hidden',
-    '&.MuiPaper-root.MuiAccordion-root': { backgroundColor: 'rgba(0, 0, 0, .03)' },
+    '&.MuiPaper-root.MuiAccordion-root': { 
+      backgroundColor: 'rgba(0, 0, 0, .03)',
+      margin: 0,
+      marginBottom: '-8px',
+    },
+    '&.MuiAccordion-root:before': { display: 'none' },
     '&:not(:last-child)': {
       borderBottom: 0,
     },
-    minHeight: '3.5rem',
+    minHeight: 'auto',
+    '& .MuiAccordionSummary-root': {
+      minHeight: 'auto',
+      padding: '8px 16px 0px 16px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    '& .MuiAccordionSummary-content': {
+      margin: '0',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    '& .MuiAccordionDetails-root': {
+      padding: '2px 16px 8px 16px',
+    },
   },
   rootExpanded: {
     overflow: 'hidden',
-    '&.MuiPaper-root.MuiAccordion-root': { backgroundColor: 'rgba(0, 0, 0, .03)' },
+    '&.MuiPaper-root.MuiAccordion-root': { 
+      backgroundColor: 'rgba(0, 0, 0, .03)',
+      margin: 0,
+      marginBottom: '-8px',
+    },
+    '&.MuiAccordion-root:before': { display: 'none' },
     flexGrow: 1,
     '& >  .MuiCollapse-root': {
       height: '100% !important',
@@ -21,6 +45,18 @@ const useStyles = makeStyles((theme) => ({
     },
     '& > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > div > .MuiAccordionDetails-root': {
       marginBottom: '5rem',
+      padding: '2px 16px 8px 16px',
+    },
+    '& .MuiAccordionSummary-root': {
+      minHeight: 'auto',
+      padding: '8px 16px 0px 16px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    '& .MuiAccordionSummary-content': {
+      margin: '0',
+      display: 'flex',
+      alignItems: 'center',
     },
   },
 }));
