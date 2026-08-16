ef8904d729e2b4e5a262666c8061129d3993dfaf
Asaf Corem <asaelcorem@gmail.com>
2026-08-16 16:57:11 +0300

    CR3 - add prod OAuth URL and INP metric

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,8 +2,9 @@ import { ReportCallback } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportCallback) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ onCLS, onFCP, onLCP, onTTFB }) => {
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry);
+      onINP(onPerfEntry);
       onFCP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
